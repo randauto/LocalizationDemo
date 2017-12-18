5285757d7e4b4fd7ec08d016c71ae8bd29dc3c6c
Le Quoc Tuan <tuanlq@vega.com.vn>
2017-12-18 09:37:09 +0700

    first commit

---
+++ LocalizationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalizationDemo
+//
+//  Created by TuanLQ on 12/18/17.
+//  Copyright © 2017 TuanLQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocalizationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalizationDemo
+//
+//  Created by TuanLQ on 12/18/17.
+//  Copyright © 2017 TuanLQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
